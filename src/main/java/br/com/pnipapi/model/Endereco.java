fa7208840a95736ccd1cff4f8f789e625a06dcb6
Joedeson Jr <joedeson.jr@gmail.com>
2023-12-03 18:27:09 -0300

    PNIP-10: Criar schema e model (backend) unidades.

--- src/main/java/br/com/pnipapi/model/Endereco.java
+++ src/main/java/br/com/pnipapi/model/Endereco.java
@@ -2,19 +2,43 @@ package br.com.pnipapi.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Table(name = "endereco", schema = "public")
+@Entity
 public class Endereco {
 
-    private int id;
-    private String uf;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column
     private String rua;
+
+    @Column
     private String cep;
-    private String pais;
-    private String bairro;
-    private String cidade;
+
+    @Column
     private String numero;
-    private String latitude;
-    private String longitude;
+
+    @Column
     private String complemento;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private String cidade;
+
+    @Column
+    private String uf;
+
+    @Column
+    private String latitude;
+
+    @Column
+    private String longitude;
 
 }
