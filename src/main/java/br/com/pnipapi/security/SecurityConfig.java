3e16c3c627efbec2858346467ca5ee14f9abfff7
matheusfriedhein <matheusfriedhein@gmail.com>
2023-12-12 09:19:04 -0300

    PNIP-12 - solicitar habilitação TR

--- src/main/java/br/com/pnipapi/security/SecurityConfig.java
+++ src/main/java/br/com/pnipapi/security/SecurityConfig.java
@@ -1,27 +1,100 @@
 package br.com.pnipapi.security;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable).cors();
+//        http.authorizeHttpRequests(httpReq -> {
+//            httpReq.requestMatchers()
+//                .hasAnyAuthority("admin")
+//                .antMatchers(HttpMethod.GET, "/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/**").permitAll()
+//                .antMatchers(HttpMethod.PUT, "/**").permitAll()
+//                .antMatchers(HttpMethod.DELETE, "/**").permitAll()
+//                .anyRequest()
+//                .permitAll();
+//        });
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        //http.oauth2ResourceServer(jwt -> jwt.jwt(j -> j.jwtAuthenticationConverter(new
+//        // CustomJwtAuthenticationConverter())));
+//        return http.build();
+//    }
+//}
+
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    private final KeycloakClientRequestFactory keycloakClientRequestFactory;
+
+    public SecurityConfig(KeycloakClientRequestFactory keycloakClientRequestFactory) {
+        this.keycloakClientRequestFactory = keycloakClientRequestFactory;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        // Padronizar rotas com hiffen. Exemplo list-disabled-users
+        http.cors()
+            .and()
+            .csrf()
+            .disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/**").permitAll()
+            .antMatchers(HttpMethod.PUT, "/**").permitAll()
+            .antMatchers(HttpMethod.DELETE, "/**").permitAll()
+            .anyRequest()
+            .authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        final var keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).cors();
-        http.authorizeHttpRequests(httpReq -> {
-            httpReq.requestMatchers()
-                    .hasAnyAuthority("admin")
-                    .anyRequest()
-                    .authenticated();
-        });
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        //http.oauth2ResourceServer(jwt -> jwt.jwt(j -> j.jwtAuthenticationConverter(new CustomJwtAuthenticationConverter())));
-        return http.build();
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }
 }
 
