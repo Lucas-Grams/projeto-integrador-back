037e666b02a62ca7aa65112b76c9c71b36167bee
Joedeson Jr <joedeson.jr@gmail.com>
2023-12-18 01:25:40 -0300

    PNIP-13: Desenvolver tabela de solicitação Acesso TR

--- src/main/java/br/com/pnipapi/service/TRService.java
+++ src/main/java/br/com/pnipapi/service/TRService.java
@@ -1,6 +1,7 @@
 package br.com.pnipapi.service;
 
 import br.com.pnipapi.dto.HabilitarTRDTO;
+import br.com.pnipapi.dto.SolicitacaoHabilitacaoDTO;
 import br.com.pnipapi.dto.documentosAPI.*;
 import br.com.pnipapi.exception.BadRequestException;
 import br.com.pnipapi.model.SolicitarHabilitacao;
@@ -13,11 +14,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class TRService {
@@ -67,6 +70,30 @@ public class TRService {
         }
     }
 
+    public List<SolicitacaoHabilitacaoDTO> minhasSolicitacoes() {
+        // TODO: id do usuario logado
+        Long idUsuario = 1L;
+        List<Object[]> results = solicitarHabilitacaoRepository.findAllSolicitacoesByIdUsuario(idUsuario);
+        if (CollectionUtils.isEmpty(results)) return null;
+        return results.stream().map(result ->
+            new SolicitacaoHabilitacaoDTO(
+                Long.parseLong(result[0].toString()),
+                Long.parseLong(result[1].toString()),
+                result[2].toString(),
+                result[3].toString()
+            )).collect(Collectors.toList());
+    }
+
+    public HabilitarTRDTO detalhesSolicitacao(String uuid) {
+        // TODO: id do usuario logado
+        Long idUsuario = 1L;
+        SolicitarHabilitacao solicitacao = solicitarHabilitacaoRepository.findSolicitacaoByIdUsuarioAndUid(idUsuario, UUID.fromString(uuid));
+        if (solicitacao != null) {
+            return convertStringToJson(solicitacao.getMetadado());
+        }
+        return null;
+    }
+
     private DespachoDTO buildDespacho(HabilitarTRDTO habilitarTRDTO) {
         DespachoDTO despachoDTO = new DespachoDTO();
         despachoDTO.setUsuarioAcao(1); // TODO REMOVER USUARIO ADMIN
@@ -104,6 +131,18 @@ public class TRService {
             mapper.registerModule(new JavaTimeModule());
             mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             return mapper.writeValueAsString(solicitarHabilitacao);
+        } catch (JsonProcessingException e) {
+            LOGGER.warn(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HabilitarTRDTO convertStringToJson(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            return mapper.readValue(json, HabilitarTRDTO.class);
         } catch (JsonProcessingException e) {
             LOGGER.warn(e.getMessage());
             throw new RuntimeException(e);
