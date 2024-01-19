ee991a8a0973d94f1568d7d4abbade97d68490d4
Lucas Gabriel Grams de Souza <lucasggsouza@hotmail.com>
2024-01-19 15:27:14 -0300

    PDSA-1760 correções solicitadas realizadas com a migration

--- src/main/resources/db/migration/V202401191535__create_table_tipo_unidade.sql
+++ src/main/resources/db/migration/V202401191535__create_table_tipo_unidade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tipo_unidade (
-                                 id BIGSERIAL PRIMARY KEY,
-                                 nome varchar(100),
-                                 tipo varchar(10)
+                              id BIGSERIAL PRIMARY KEY,
+                              nome varchar(100),
+                              tipo varchar(10)
 );
 
 INSERT INTO public.tipo_unidade (nome, tipo) VALUES ('Unidade Central (UC)', 'UC');
@@ -12,3 +12,16 @@ INSERT INTO public.tipo_unidade (nome, tipo) VALUES ('Ministério da Pesca e Aqu
 INSERT INTO public.tipo_unidade (nome, tipo) VALUES ('Secretaria Nacional (SN)', 'SN');
 INSERT INTO public.tipo_unidade (nome, tipo) VALUES ('Departamento (DP)', 'DP');
 INSERT INTO public.tipo_unidade (nome, tipo) VALUES ('Superintendencia Federal da Pesca (SFP)', 'SFP')
+
+ALTER TABLE unidade_usuario
+ALTER COLUMN id TYPE bigint;
+
+ALTER TABLE unidade_usuario
+DROP CONSTRAINT unidade_usuario_pkey;
+
+ALTER TABLE unidade_usuario
+    ADD PRIMARY KEY (id_unidade, id_usuario, id_permissao);
+
+
+INSERT INTO permissao (descricao) VALUES ('so');
+INSERT INTO permissao (descricao) VALUES ('representante');
