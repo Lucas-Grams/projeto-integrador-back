7b448a667fe998e627104bd8f4370faa2d098e5c
matheusfriedhein <matheusfriedhein@gmail.com>
2024-01-20 00:17:53 -0300

    identação unidade e unidadeUsuarios

--- src/main/java/br/com/pnipapi/repository/UnidadeRepository.java
+++ src/main/java/br/com/pnipapi/repository/UnidadeRepository.java
@@ -1,10 +1,12 @@
 package br.com.pnipapi.repository;
+
 import br.com.pnipapi.model.Unidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,15 +18,13 @@ public interface UnidadeRepository extends JpaRepository<Unidade, Long> {
         "   WHERE u.tipo = :tipo AND ativo = true", nativeQuery = true)
     List<Unidade> findUnidadesByTipo(@Param("tipo") String tipo);
 
-
     @Query(value = "SELECT * FROM unidade u " +
         "  WHERE u.uuid = CAST(:uuid AS UUID)", nativeQuery = true)
     Unidade findUnidadeByUuid(@Param("uuid") String uuid);
 
-
     @Modifying
-    @Query(value="UPDATE unidade_usuario SET ativo =:ativo  WHERE id_unidade =:idUnidade", nativeQuery = true)
-    void updateUsuariosByIdUnidade(long idUnidade, boolean  ativo);
+    @Query(value = "UPDATE unidade_usuario SET ativo =:ativo  WHERE id_unidade =:idUnidade", nativeQuery = true)
+    void updateUsuariosByIdUnidade(long idUnidade, boolean ativo);
 
     Long countUnidadeByNomeAndAtivo(@Param("nome") String nome, @Param("ativo") boolean ativo);
 
